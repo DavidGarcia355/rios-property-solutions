a798d85d6390a95f68e2ca5e0f07443a5331d62a
DavidGarcia355 <119974000+DavidGarcia355@users.noreply.github.com>
2026-01-14 00:11:09 -0600

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// Optional: highlight current nav link
-(function(){
-  const path = location.pathname.split("/").pop() || "index.html";
-  document.querySelectorAll("nav a[data-page]").forEach(a=>{
-    if(a.getAttribute("data-page") === path) a.style.color = "var(--text)";
-  });
-})();
